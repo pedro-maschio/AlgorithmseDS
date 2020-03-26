54d6b7b865054c44b78b7770754d8ccf36f8d779
Pedro Maschio <pedrotorres_maschio@hotmail.com>
2020-03-26 20:14:25 -0300

    Added one question

---
+++ URI/credoparatudo.cpp
@@ -0,0 +1,92 @@
+/*
+    Solution for the problem 2959 from URI Online Judge
+    https://www.urionlinejudge.com.br/judge/pt/problems/view/2959
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph {
+    int V;
+    list<int> *adj;
+    public:
+        Graph(int V);
+        void addEdge(int v, int u);
+        bool isReachable(int s, int d);
+        void printGraph();
+};
+
+void Graph::printGraph() {
+    for(int i = 0; i < this->V; i++) {
+        cout << "Adjacency list of " << i << " element: ";
+        
+        for(auto a : adj[i])
+            cout << "-> " << a;
+        cout << endl;
+    }
+}
+
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+bool Graph::isReachable(int s, int d) {
+    if(s == d)
+        return true;
+    
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++) {
+        visited[i] = false;
+    }
+
+    list<int> queue;
+
+    visited[s] = true;
+    queue.push_back(s);
+
+    list<int>::iterator i;
+
+    while(!queue.empty()) {
+        s = queue.front();
+        queue.pop_front();
+
+        for(i = adj[s].begin(); i != adj[s].end(); i++) {
+            if(*i == d)
+                return true;
+            else if(!visited[*i]) {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+    return false;
+}
+
+
+int main() {
+    int V, E, Q, u, v;
+
+    cin >> V >> E >> Q;
+
+    Graph graph(V+1); // to become 1-indexed
+    for(int i = 0; i < E; i++) {
+        cin >> u >> v;
+        graph.addEdge(u, v);
+    }
+
+    //graph.printGraph();
+    for(int i= 0;i < Q; i++) {
+        cin >> u >> v;
+        if(graph.isReachable(u, v))
+            cout << "Lets que lets" << endl;
+        else 
+            cout << "Deu ruim" << endl;
+    }
+
+    return 0;
+}
